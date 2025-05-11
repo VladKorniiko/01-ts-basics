b10a24c2a700746889ddcb39790ae05ae792de9d
Vlad <korniyko1911@gmail.com>
2025-05-12 00:42:03 +0300

    dz fix

--- src/task-6.ts
+++ src/task-6.ts
@@ -5,4 +5,3 @@ function getFirstElement<T>(arr: T[]): T {
 getFirstElement<number>([1, 2, 3]); // 1
 getFirstElement<string>(["a", "b", "c"]); // "a"
 getFirstElement<boolean>([true, false, true]); // true
-getFirstElement<number>([1, 2, "string"]);
