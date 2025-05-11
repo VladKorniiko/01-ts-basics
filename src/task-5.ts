2df45f8edb2a00843d8c41d1af01f76e1afe7fa1
Vlad <korniyko1911@gmail.com>
2025-05-11 17:20:07 +0300

    dz

---
+++ src/task-5.ts
@@ -0,0 +1,14 @@
+function logStatus(status: Status): void {
+  if (status === "loading") {
+    console.log("Loading...");
+  } else if (status === "success") {
+    console.log("Success!");
+  } else if (status === "error") {
+    console.log("Something went wrong");
+  }
+}
+
+type Status = "loading" | "success" | "error";
+logStatus("loading");
+logStatus("success");
+logStatus("error");
